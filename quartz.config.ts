89521237b79d5d8b8518ad54d92173ff11f8a092
StorylineUnited <info@storylinesunited.com>
2026-04-18 13:06:35 -0400

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "churchstreetwiki.netlify.app/blogs/wiki/",
+    baseUrl: "churchstreetwiki.netlify.app/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
